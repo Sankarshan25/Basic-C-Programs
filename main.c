bbc093449ef9a9e1c0636b419aaf855a0cd6662b
Sankarshan Kundu <91846719+Sankarshan25@users.noreply.github.com>
2021-10-21 23:24:54 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number(1-7):\t");
+    scanf("%d",&n);
+    switch(n)
+    {
+    case 1:
+        printf("\nSunday");
+        break;
+    case 2:
+        printf("\nMonday");
+        break;
+    case 3:
+        printf("\nTuesday");
+        break;
+    case 4:
+        printf("\nWednesday");
+        break;
+    case 5:
+        printf("\nThursday");
+        break;
+    case 6:
+        printf("\nFriday");
+        break;
+    case 7:
+        printf("\nSaturday");
+        break;
+    }
+    return 0;
+}

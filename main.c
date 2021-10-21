34fbecae777a695b5b48fe9144497d160e4ba7ca
Sankarshan Kundu <91846719+Sankarshan25@users.noreply.github.com>
2021-10-21 23:20:28 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,D,d,x,y;
+    printf("Enter the values of a,b,c:\n");
+    scanf("%d %d %d",&a,&b,&c);
+    d=2*a;
+    D=(b*b-4*a*c);
+    printf("\nD=%d",D);
+    x=(-b+sqrt(D)) /d;
+    y=(-b-sqrt(D))/d;
+    printf("\nx=%d y=%d",x,y);
+    if(D>0)
+    printf("\nThe roots are real and unequal.");
+    if(D==0)
+        printf("\nThe roots are real and equal.");
+    else if (D<0)
+        printf("\nThe roots are imaginary.");
+
+
+    return 0;
+}

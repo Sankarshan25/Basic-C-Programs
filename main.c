ace53777224f95770d6fab14bbfd69304551ed6f
Sankarshan Kundu <91846719+Sankarshan25@users.noreply.github.com>
2021-10-22 00:14:58 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[10][10],i,j,rows=0,columns=0;
+    printf("Enter no of rows:\t ");
+    scanf("%d",&rows);
+    printf("\nEnter no of columns:\t");
+    scanf("%d",&columns);
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<columns;j++)
+        {
+            printf("\nEnter a[%d][%d] element of matrix:\t",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("\nThe elements of the matrix are:\n");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<columns;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}

83984ae7ef1682aee9676804b2c6c7003925fd9f
Sankarshan Kundu <91846719+Sankarshan25@users.noreply.github.com>
2021-10-21 23:22:29 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    int lowercase,uppercase;
+    printf("Enter a letter:\t");
+    scanf("%c",&c);
+    lowercase=(c=='a' || c=='e'|| c=='i'|| c=='o'|| c=='u');
+    uppercase=(c=='A' || c=='E'|| c=='I'|| c=='O'|| c=='U');
+    if(lowercase||uppercase)
+        printf("The letter is a vowel.");
+    else
+        printf("The letter is a consonant.");
+    return 0;
+}
